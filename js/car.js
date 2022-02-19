4d21c9bafdbc2dead5f8c7cbff6a187f3d8a3afe
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2022-02-19 09:14:41 +0200

    Added Playable Player 2

--- js/car.js
+++ js/car.js
@@ -10,36 +10,56 @@ function carClass() {
 	this.y = 75;
 	this.ang = 0;
 	this.speed = 0;
+	this.myCarPic; //Which Picture to use
 
-	this.reset = function() {
-		for(var eachRow=0;eachRow<TRACK_ROWS;eachRow++) {
-			for(var eachCol=0;eachCol<TRACK_COLS;eachCol++) {
-				var arrayIndex = rowColToArrayIndex(eachCol, eachRow); 
-				if(trackGrid[arrayIndex] == TRACK_PLAYERSTART) {
+	this.keyHeld_Gas = false;
+	this.keyHeld_Reverse = false;
+	this.keyHeld_TurnLeft = false;
+	this.keyHeld_TurnRight = false;
+
+	this.controlKeyUp;
+	this.controlKeyRight;
+	this.controlKeyDown;
+	this.controlKeyLeft;
+
+	this.setupInput = function (upKey, rightKey, downKey, leftKey) {
+		this.controlKeyUp = upKey;
+		this.controlKeyRight = rightKey;
+		this.controlKeyDown = downKey;
+		this.controlKeyLeft = leftKey;
+	}
+
+	this.reset = function (whichImage) {
+		this.myCarPic = whichImage;
+
+		for (var eachRow = 0; eachRow < TRACK_ROWS; eachRow++) {
+			for (var eachCol = 0; eachCol < TRACK_COLS; eachCol++) {
+				var arrayIndex = rowColToArrayIndex(eachCol, eachRow);
+				if (trackGrid[arrayIndex] == TRACK_PLAYERSTART) {
 					trackGrid[arrayIndex] = TRACK_ROAD;
-					this.ang = -Math.PI/2;
-					this.x = eachCol * TRACK_W + TRACK_W/2;
-					this.y = eachRow * TRACK_H + TRACK_H/2;
+					this.ang = -Math.PI / 2;
+					this.x = eachCol * TRACK_W + TRACK_W / 2;
+					this.y = eachRow * TRACK_H + TRACK_H / 2;
 					return;
 				} // end of player start if
 			} // end of col for
 		} // end of row for
 	} // end of carReset func
 
-	this.move = function() {
+	this.move = function () {
 		this.speed *= GROUNDSPEED_DECAY_MULT;
 
-		if(keyHeld_Gas) {
+		if (this.keyHeld_Gas) {
 			this.speed += DRIVE_POWER;
 		}
-		if(keyHeld_Reverse) {
+		if (this.keyHeld_Reverse) {
 			this.speed -= REVERSE_POWER;
 		}
-		if(Math.abs(this.speed) > MIN_SPEED_TO_TURN) {
-			if(keyHeld_TurnLeft) {
+		if (Math.abs(this.speed) > MIN_SPEED_TO_TURN) {
+			if (this.keyHeld_TurnLeft) {
 				this.ang -= TURN_RATE;
 			}
-			if(keyHeld_TurnRight) {
+			if (this.keyHeld_TurnRight) {
 				this.ang += TURN_RATE;
 			}
 		}
@@ -50,7 +70,7 @@ function carClass() {
 		carTrackHandling(this);
 	}
 
-	this.draw = function() {
-		drawBitmapCenteredWithRotation(carPic, this.x,this.y, this.ang);
+	this.draw = function () {
+		drawBitmapCenteredWithRotation(this.myCarPic, this.x, this.y, this.ang);
 	}
 }
